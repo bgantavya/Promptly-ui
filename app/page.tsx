11723d074361a3091dd45cd763d30c356ac3bc78
bgantavya <147243873+bgantavya@users.noreply.github.com>
2025-05-12 15:03:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "../public/next.svg";
+import ConsolidateSection from "./components/ConsolidateSection/page";
+import TestSection from "./components/TestSection/page";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-items-center p-8 font-[family-name:var(--font-geist-sans)]">
+  <TestSection/>
+      <ConsolidateSection  />
+  <TestSection/>
+
+    </div>
+  );
+}
